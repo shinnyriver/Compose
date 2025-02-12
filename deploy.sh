f70298da0ceba8f289483b8bed036d26a568d7ce
Changsu Park <gd12091@gmail.com>
2025-02-12 15:24:42 +0900

    test: CI

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,11 @@ git pull origin main
 
 # 기존 컨테이너 중지 및 삭제
 docker-compose down
-DOCKER_USERNAME=${DOCKER_USERNAME}
+echo "DOCKER_USERNAME=${DOCKER_USERNAME}" > .env
+echo "SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}" >> .env
+echo "SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}" >> .env
+echo "SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}" >> .env
+
 # ✅ Docker Hub에서 최신 이미지 가져오기
 docker pull $DOCKER_USERNAME/mysql:latest
 docker pull $DOCKER_USERNAME/backend:latest
