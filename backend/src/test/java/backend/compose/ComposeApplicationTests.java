d9c892435c0ce513dd3c44d93e9c7546c72feff6
Changsu Park <gd12091@gmail.com>
2025-02-12 09:24:12 +0900

    Initial Commit

---
+++ backend/src/test/java/backend/compose/ComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.compose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
